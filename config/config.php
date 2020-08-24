71779632e88276b553d72f9eb294f770ba9c1ad5
Muhammad Hannan <mdhannan.info@gmail.com>
2020-08-24 18:02:25 +0600

    db connection

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "blg");
+define("DB_HOST", "sql12.freemysqlhosting.net");
+define("DB_USER", "sql12362034");
+define("DB_PASS", "4ExgHxKntG");
+define("DB_NAME", "sql12362034");
 define("TITLE", "MH Blog");
 define("META_DESC", "A blog develop by Muhammad Hannan using PHP & MySQL.");
 define("KEYWORDS", "PHP, Laravel,Vue Js, Wordpress, plugin");
